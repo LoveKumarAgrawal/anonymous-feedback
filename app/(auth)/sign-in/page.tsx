59e0b2ce34e107dc1eb278206505ccd20675d607
LoveKumarAgrawal <loveagrawal204@gmail.com>
2025-01-13 20:43:39 +0530

    init shadcn and signin page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useState } from "react"
+
+const Page = () => {
+    const [username, setUsername] = useState("")
+    const [usernameMessage, setUsernameMessage] = useState("")
+    const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    return (
+        <div>page</div>
+    )
+}
+
+export default Page
